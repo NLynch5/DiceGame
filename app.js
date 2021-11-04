9133f71fb5de837dae938133e1b0ff9167f2ca98
John Doe <natalieclairelynch@gmail.com>
2021-11-04 14:34:11 +0000

    css and html updated

---
+++ app.js
@@ -0,0 +1,29 @@
+//dice 
+const dice1 = document.getElementById("dice1");
+const dice2 = document.getElementById("dice2");
+const dice3 = document.getElementById("dice3");
+const dice4 = document.getElementById("dice4");
+const dice5 = document.getElementById("dice5");
+const dice6 = document.getElementById("dice6");
+
+//page
+const button = document.getElementById ("button");
+const diceImage = document.getElementById ("diceImage");
+
+
+button.addEventListener ("click" ()=> {
+    if (????? == 1){
+        document.getElementById ("dice1");
+    } if else {
+        ???? == 2
+    } if else {
+        
+    }
+})
+
+
+
+
+
+
+
