60e6d8bb2c77aae0a115ea9495659aed5c417b13
John Doe <natalieclairelynch@gmail.com>
2021-11-08 18:33:55 +0000

    updated js

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 // dice
-let dice1 = document.getElementById("dice1");
-let dice2 = document.getElementById("dice2");
-let dice3 = document.getElementById("dice3");
-let dice4 = document.getElementById("dice4");
-let dice5 = document.getElementById("dice5");
-let dice6 = document.getElementById("dice6");
+// let dice1 = document.getElementById("dice1");
+// let dice2 = document.getElementById("dice2");
+// let dice3 = document.getElementById("dice3");
+// let dice4 = document.getElementById("dice4");
+// let dice5 = document.getElementById("dice5");
+// let dice6 = document.getElementById("dice6");
 
 //page
-let score = document.getElementById ("score");
+let roll = document.getElementById ("roll");
 let diceImage = document.getElementById ("diceImage");
 let button = document.getElementById("button");
 
 
-function rollDice (){
-        let roll = Math.floor(Math.random() * 6) + 1;
-        score.innerHTML = roll;     
-}
-button.addEventListener ("click", () => { 
-        if (score === 1){
-        $("#diceImage").attr(src=Images/dice1.svg);
-    } else if (score === 2){
-        $("diceImage").attr("src",`Images/dice2.svg`);
-    } else if (score === 3){
-        $("diceImage").attr("src", `Images/dice3.svg`);
-    } else if (score === 4){
-        $("diceImage").attr("src", `Images/dice4.svg`);
-    } else if (score === 5){
-        $("diceImages").attr("src",`Images/dice5.svg`);
-    } else if (score == 6){
-        $("diceImages").attr("src", `Images/dice6.svg`);
+ //function rollDice (){
+   //    let roll = Math.floor(Math.random() * 6) + 1;
+     //   score.innerHTML = roll;  
+
+//button.addEventListener ("click", function () {   
+//})
+//}
+
+button.addEventListener("click", () => {
+    let num = Math.floor(Math.random() * 6) + 1;
+    roll.innerHTML = num;  
+   if (num === 1) {
+        diceImage.src = "Images/dice1.svg";
+    } else if (num === 2){
+        diceImage.style.transform ("src",`Images/dice2.svg`);
+    } else if (num === 3){
+        diceImage.attr("src",`Images/dice3.svg`);
+    } else if (num === 4){
+        diceImage.attr("src",`Images/dice4.svg`);
+    } else if (num === 5){
+        diceImage.attr("src",`Images/dice5.svg`);
+    } else if (num === 6){
+        diceImage.attr(src=Images/dice6.svg);
     }
 })
+
