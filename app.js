38b0b547e4ff86f1b8961bcf6c2a89a7015c3cea
Natalie Lynch <natalielynch@Natalies-MacBook-Air.local>
2021-12-16 17:14:44 +0000

    updated function however still not working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //page
-let roll = document.getElementById("roll");
+//let roll = document.getElementById("roll");
 let diceImage = document.getElementById("diceImage");
 let button = document.getElementById("button");
 let score = document.getElementById("score");
@@ -10,25 +10,29 @@ button.addEventListener("click", () => {
   let num = Math.floor(Math.random() * 6) + 1;
   score.innerHTML = num;
   if (num === 1) {
-    result.innerHTML = 1;
+    rollResult += num;
     diceImage.src = "Images/dice1.svg";
+    //total.innerHTML += num;
   } else if (num == 2) {
-    result.innerHTML += 2;
-    total.innerHTML += 2;
     diceImage.src = "Images/dice2.svg";
+    //rollResult += 2;
+    //total.innerHTML = num + rollResult;
   } else if (num == 3) {
-    result.innerHTML += 3;
+    //rollResult.innerHTML += 3;
+    //total.innerHTML = num + 3;
     diceImage.src = "Images/dice3.svg";
   } else if (num == 4) {
-    result.innerHTML += 4;
+    //rollResult.innerHTML += 4;
+    //total.innerHTML = num + 4;
     diceImage.src = "Images/dice4.svg";
   } else if (num == 5) {
-    result.innerHTML += 5;
+    //rollResult.innerHTML += 5;
+    //total.innerHTML = num + 5;
     diceImage.src = "Images/dice5.svg";
   } else if (num == 6) {
-    result.innerHTML += 6;
+    //rollResult.innerHTML += 6;
+    //total.innerHTML = num + 6;
     diceImage.src = "Images/dice6.svg";
     //} else if (total >= 25) {
-    //  heading.innerHTML = "Winner";
   }
 });
