2a5a2017cdc64625642f84e1eea7aab119ee2c22
Natalie Lynch <natalielynch@Natalies-MacBook-Air.local>
2021-12-17 16:57:53 +0000

    fixed code for the roll count and total for winner

--- app.js
+++ app.js
@@ -3,36 +3,45 @@
 let diceImage = document.getElementById("diceImage");
 let button = document.getElementById("button");
 let score = document.getElementById("score");
-let total = 0;
+let total = document.getElementById("total");
+let playBtn = document.getElementById("playBtn");
+let count = 0;
 
 //button function to create random number and change image
 button.addEventListener("click", () => {
   let num = Math.floor(Math.random() * 6) + 1;
-  score.innerHTML = num;
+  //score.innerHTML = num;
   if (num === 1) {
-    rollResult += num;
     diceImage.src = "Images/dice1.svg";
-    //total.innerHTML += num;
+    count += 1;
+    score.textContent = `${count}`;
   } else if (num == 2) {
     diceImage.src = "Images/dice2.svg";
-    //rollResult += 2;
-    //total.innerHTML = num + rollResult;
+    count += 2;
+    score.textContent = `${count}`;
   } else if (num == 3) {
-    //rollResult.innerHTML += 3;
-    //total.innerHTML = num + 3;
     diceImage.src = "Images/dice3.svg";
+    count += 3;
+    score.textContent = `${count}`;
   } else if (num == 4) {
-    //rollResult.innerHTML += 4;
-    //total.innerHTML = num + 4;
     diceImage.src = "Images/dice4.svg";
+    count += 4;
+    score.textContent = `${count}`;
   } else if (num == 5) {
-    //rollResult.innerHTML += 5;
-    //total.innerHTML = num + 5;
     diceImage.src = "Images/dice5.svg";
+    count += 5;
+    score.textContent = `${count}`;
   } else if (num == 6) {
-    //rollResult.innerHTML += 6;
-    //total.innerHTML = num + 6;
     diceImage.src = "Images/dice6.svg";
-    //} else if (total >= 25) {
+    count += 6;
+    score.textContent = `${count}`;
   }
+  if (count >= 25) {
+    score.textContent = `You win`;
+    diceImage.src = "Images/trophy.png";
+  }
+});
+
+playBtn.addEventListener("click", () => {
+  count = 0;
 });
